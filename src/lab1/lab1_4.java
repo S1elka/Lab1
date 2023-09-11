5edad87202188acd4662436568713a353def862c
S1elka <juravchik2005@mail.ru>
2023-09-12 01:36:27 +0300

    check

---
+++ src/lab1/lab1_4.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class lab1_4 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) {
+            System.out.format("%.3f ", 1./i);
+        }
+    }
+}
